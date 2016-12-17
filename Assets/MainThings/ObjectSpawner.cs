d70558e97c2f0b399b797fd89291cff5b1338a63
Liam Sorta <liamsorta@gmail.com>
2016-12-17 21:20:23 +0000

    MainThingsAndSuch
    Yes

---
+++ Assets/MainThings/ObjectSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour {
+
+    [SerializeField]
+    GameObject[] gifts;
+
+    [SerializeField]
+    Transform[] points;
+
+    private GameObject player;
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+	void Update () {
+        //if (Input.GetKeyDown(KeyCode.A))
+            FireGift(Random.Range(0, points.Length));
+    }
+	
+	void FireGift(int i) {
+        GameObject gift = (GameObject)Instantiate(gifts[i], new Vector3(points[0].position.x, points[0].position.y, Random.Range(points[0].position.z, points[1].position.z)), Quaternion.identity);
+        gift.AddComponent<PrezzieLogic>();
+    }
+}
