22698fc66d86167f53aa4fa3487fb6437338b204
Liam Sorta <liamsorta@gmail.com>
2016-12-18 14:03:05 +0000

    sad cats
    yes

--- Assets/PlayerStats.cs
+++ Assets/PlayerStats.cs
@@ -6,15 +6,24 @@ public class PlayerStats : MonoBehaviour {
 
     float health;
 
+    public List<GameObject> cats = new List<GameObject>();
+
     public void TakeDamage(float damage) {
         health -= damage;
         //updateHP
     }
 
+    private void Start() {
+        foreach(GameObject ob in GameObject.FindGameObjectsWithTag("cat")) {
+            cats.Add(ob);
+        }
+    } 
+
     //DO PHYSICS CHECKS
 
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.tag == "gift") {
+            Destroy(cats[Random.Range(0, cats.Count)]);
             Destroy(collision.gameObject);
         }
     }
