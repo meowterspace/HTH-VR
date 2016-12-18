0f37d4a8dbf978c518aeb067291bd480aba24da1
Ben <ben@bromleycroft.co.uk>
2016-12-18 02:26:16 +0000

    meta fix

--- Assets/MainThings/ObjectSpawner.cs
+++ Assets/MainThings/ObjectSpawner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour {
+	float lastTime = 0;
 
     [SerializeField]
     GameObject[] gifts;
@@ -17,9 +18,12 @@ public class ObjectSpawner : MonoBehaviour {
     }
 
 	void Update () {
-        //if (Input.GetKeyDown(KeyCode.A))
-            FireGift(Random.Range(0, points.Length));
-    }
+		//if (Input.GetKeyDown(KeyCode.A))
+		if (lastTime + 1 < Time.time) { 
+			FireGift (Random.Range (0, points.Length));
+			lastTime = Time.time;
+		}
+	}
 	
 	void FireGift(int i) {
         GameObject gift = (GameObject)Instantiate(gifts[i], new Vector3(points[0].position.x, points[0].position.y, Random.Range(points[0].position.z, points[1].position.z)), Quaternion.identity);
