3c38565010bc23e3eca5128098b5465db786abf3
Liam Sorta <liamsorta@gmail.com>
2016-12-18 13:44:19 +0000

    tHANK GOD ITS CHRISTMAS
    yesss

---
+++ Assets/FLIPTHETABLEMATE.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FLIPTHETABLEMATE : MonoBehaviour {
+    float timemate;
+    // Use this for initialization
+    void Start() {
+        timemate = Time.time;
+        StartCoroutine(poop());
+    }
+
+    IEnumerator poop() {
+        yield return new WaitForSeconds(30);
+        GetComponent<MeshRenderer>().enabled = true;
+        yield return new WaitForSeconds(15);
+        GetComponent<MeshRenderer>().enabled = false;
+
+
+    }
+}
